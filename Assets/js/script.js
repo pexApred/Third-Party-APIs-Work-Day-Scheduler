01982a371967d2a5c96483ca9a11f162f11daa24
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-26 21:33:23 -0500

    added code to display current date in the header

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +21,12 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function updateDate () {
+    var currentDate = dayjs().format('dddd, MMMM DD[th]');
+    $('#currentDay').text(currentDate);
+  }
+
+  // Update the date every day
+  setInterval ( updateDate, 24 * 60 * 60 * 1000 )
+
 });
